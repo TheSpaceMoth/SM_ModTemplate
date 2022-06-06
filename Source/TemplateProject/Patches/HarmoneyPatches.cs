eac23a47d43057fb8faa18d000d3ef4a70114c71
TheSpaceMoth <bellsab3000@gmail.com>
2022-06-06 14:16:00 +0100

    Update for initial version of template

---
+++ Source/TemplateProject/Patches/HarmoneyPatches.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using HarmonyLib;
+using RimWorld;
+using RimWorld.Planet;
+using Verse;
+using Verse.AI;
+using Verse.Sound;
+
+namespace TemplateProject
+{
+    [StaticConstructorOnStartup]
+    static public class HarmonyPatches
+    {
+        public static Harmony harmonyInstance;
+
+
+        static HarmonyPatches()
+        {
+            harmonyInstance = new Harmony("rimworld.rwmods.TemplateProject");
+            harmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+    }
+
+
+    [HarmonyPatch(typeof(Type))]
+    [HarmonyPatch("Function")]
+    internal static class DrawStaticButton
+    {
+        public static bool Prefix(ref Letter __instance, float topY)
+        {
+            __instance.def.bounce = false;          // Never bounce
+            __instance.def.flashInterval = 0.0f;    // No flashing
+            return true; // Use old code
+        }
+    }
+
+}
